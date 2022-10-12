33f378188cb704f4d14b2dcb04e336964e0d9fa5
myelyn <myelyn@163.com>
2022-10-12 17:11:02 +0800

    vite config

--- cashbook-web/vite.config.js
+++ cashbook-web/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { createStyleImportPlugin } from 'vite-plugin-style-import'
 import path from 'path'
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     createStyleImportPlugin(
